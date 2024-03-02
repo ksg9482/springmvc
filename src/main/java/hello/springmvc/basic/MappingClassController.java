5f588c1d441455a68bcfed590ad2677ae1d69df2
ksg9482 <ksg930523@gmail.com>
2024-03-02 23:09:54 +0900

    start springmvc - params

---
+++ src/main/java/hello/springmvc/basic/MappingClassController.java
@@ -0,0 +1,34 @@
+package hello.springmvc.basic;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+
+    //회원 목록 조회
+    @GetMapping
+    public String user() {
+        return "get users";
+    };
+
+    @PostMapping
+    public String addUser() {
+        return "post user";
+    }
+
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable String userId) {
+        return "get userId = " + userId;
+    }
+
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable String userId) {
+        return "update userId = " + userId;
+    }
+
+    @DeleteMapping("/mapping/users/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        return "delete userId = " + userId;
+    }
+}
---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,83 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username={}, age={}", username, age);
+
+        response.getWriter().write("ok");
+    }
+
+    @ResponseBody //RestController 같은 역할
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge
+    ) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";//Controller는 그냥 문자열이면 뷰 리졸버를 찾는다. 컨트롤러 자체가 아니라 내부 매핑에서 처리하려면 @ResponseBody 이용.
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age
+    ) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) { //요청 파라이터 이름과 같으면 아예 @RequestParam 제거 가능
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            //required = false Optional 설정 같은 느낌
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age
+    ) { //required 설정 넣으려면 @RequestParam 넣어야 한다,
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age
+    ) { //defaultValue가 들어가게 되면 사실상 required가 필요없음
+        //빈문자가 와도 defaultValue가 동작하는 점이 특이사항
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam() Map<String, Object> paramMap) { //defaultValue가 들어가게 되면 사실상 required가 필요없음
+        //빈문자가 와도 defaultValue가 동작하는 점이 특이사항
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+}
